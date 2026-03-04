fb9ca03298725c78c09a29e21bc0c9f7de8806fc
Kuse_design <0luwakusejerry1@gmail.com>
2026-03-04 11:02:06 +0100

    jerry commit

--- src/data/models/GatePass.java
+++ src/data/models/GatePass.java
@@ -1,4 +1,60 @@
-package models;
+package data.models;
+
+import java.time.LocalDateTime;
 
 public class GatePass {
+    private int id;
+    private int residentId;
+    private int visitorId;
+    private LocalDateTime createdAt =  LocalDateTime.now();
+    private LocalDateTime expiresAt;
+    private boolean isValid = true;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getResidentId() {
+        return residentId;
+    }
+
+    public void setResidentId(int residentId) {
+        this.residentId = residentId;
+    }
+
+    public int getVisitorId() {
+        return visitorId;
+    }
+
+    public void setVisitorId(int visitorsId) {
+        this.visitorId = visitorsId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
+    }
 }
